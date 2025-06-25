1689e249b48f8c9543ef8917cf1a09db6f5f964f
kimyuchan <rladbcks103@naver.com>
2025-06-25 19:44:31 +0900

    first commit

---
+++ src/main/java/patrick/til_backend/TilBackendApplication.java
@@ -0,0 +1,13 @@
+package patrick.til_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/patrick/til_backend/TilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package patrick.til_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
